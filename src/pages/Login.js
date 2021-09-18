5973430d2776601efb17d9970265a81c23f64f67
Prashant <prashanttechjha@gmail.com>
2021-09-18 16:54:20 +0530

    netlify error

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,55 +1,55 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Login = ({ history }) => {
-    const [userName, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [userName, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const loginHandler = async (e) => {
-        e.preventDefault();
+  const loginHandler = async (e) => {
+    e.preventDefault();
 
-        const config = {
-            header: {
-                "Content-Type": "application/json",
-            },
-        };
-
-        try {
-            const { data } = await axios.post(
-                "/auth/login",
-                { userName, password },
-                config
-            );
-
-            localStorage.setItem("authToken", data.token);
-
-            history.push("/profile");
-        } catch (error) {
-            console.log(error.message);
-        }
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
     };
 
-    return (
-        <div>
-            <form onSubmit={loginHandler} >
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="username"
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Login</button>
-            </form>
-            <Link to="/password-reset" >Forgot password?</Link>
-        </div>
-    )
-}
+    try {
+      const { data } = await axios.post(
+        "https://cunnette.herokuapp.com/auth/login",
+        { userName, password },
+        config
+      );
 
-export default Login
+      localStorage.setItem("authToken", data.token);
+
+      history.push("/profile");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={loginHandler}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <Link to="/password-reset">Forgot password?</Link>
+    </div>
+  );
+};
+
+export default Login;
